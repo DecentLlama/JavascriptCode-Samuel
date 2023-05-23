764653020ee79503f3976ede4fd9d571411a39b0
Samuel Mayorga <128614074+DecentLlama@users.noreply.github.com>
2023-05-23 09:04:54 -0600

    Operacion con numeros

--- Javascript code- Samuel Mayorga/js/main.js
+++ Javascript code- Samuel Mayorga/js/main.js
@@ -1,1 +1,31 @@
 console.log("Hello World!");
+
+//alert("Bienvenido a Javascript");
+/*
+Comentario Multilinea
+
+let nombre = prompt("¿Cual es tu nombre?");
+
+console.log(parseInt(nombre));
+
+console.log(typeof(nombre));
+*/
+
+let numeroUno = prompt("Ingresa un número");
+let numeroDos = prompt("Ingresa otro número");
+
+let resultadoSuma = parseInt(numeroUno) + parseInt(numeroDos);
+
+console.log(`El resultado de la suma es: ${resultadoSuma}`);
+
+let resultadoResta = parseInt(numeroUno) - parseInt(numeroDos);
+
+console.log(`El resultado de la resta es: ${resultadoResta}`);
+
+let resultadoMultiplicacion = parseInt(numeroUno) * parseInt(numeroDos);
+
+console.log(`El resultado de la multiplicacion es: ${resultadoMultiplicacion}`);
+
+let resultadoDivision = parseInt(numeroUno) / parseInt(numeroDos);
+
+console.log(`El resultado de la division es: ${resultadoDivision}`);
