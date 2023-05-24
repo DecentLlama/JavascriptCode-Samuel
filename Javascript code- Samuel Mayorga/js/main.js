ddb6ae04a15d8c0f98d7348eef4413ee1e06e69f
Samuel Mayorga <128614074+DecentLlama@users.noreply.github.com>
2023-05-24 09:07:18 -0600

    Condicional

--- Javascript code- Samuel Mayorga/js/main.js
+++ Javascript code- Samuel Mayorga/js/main.js
@@ -9,8 +9,8 @@ let nombre = prompt("¿Cual es tu nombre?");
 console.log(parseInt(nombre));
 
 console.log(typeof(nombre));
-*/
 
+ Clase 23/05/2023{
 let numeroUno = prompt("Ingresa un número");
 let numeroDos = prompt("Ingresa otro número");
 
@@ -28,4 +28,31 @@ console.log(`El resultado de la multiplicacion es: ${resultadoMultiplicacion}`);
 
 let resultadoDivision = parseInt(numeroUno) / parseInt(numeroDos);
 
-console.log(`El resultado de la division es: ${resultadoDivision}`);
+console.log(`El resultado de la division es: ${resultadoDivision}`); 
+ }
+*/
+
+//Dados dos numeros identificar cual numero es mayor
+//cual es menor o si son iguales
+
+/*let numeroUno = prompt("Ingresa un numero");
+let numeroDos = prompt("Ingresa otro numero");
+
+if(numeroUno>numeroDos){
+    console.log(`El numero ${numeroUno} es mayor que ${numeroDos}`);
+}else if(numeroDos>numeroUno) {
+    console.log(`El numero ${numeroDos} es mayor que ${numeroUno}`);
+}
+else{
+    console.log(`El numero ${numeroUno} es igual a ${numeroDos}`);
+}
+*/
+//Dado un numero identificar si un numero es par o impar
+
+let numeroUno = prompt("Ingresa el valor deseado");
+let resultado = parseInt(numeroUno) % 2;
+if(resultado==0){
+    console.log(`El numero ${numeroUno} es par`);
+}else{
+    console.log(`El numero ${numeroUno} es impar`);
+}
